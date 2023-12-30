31e28d3cde7aebe3d3917b3a309e6011d326faf5
bitWolfy <anon.wolfy@pm.me>
2023-12-30 14:17:57 -0800

    Update the moderator script

--- bin/moderators.js
+++ bin/moderators.js
@@ -1,12 +1,17 @@
 
 //// CONFIG START
 
-const expectation = 10;                 // Expected number of DAILY actions
 const exemptUsers = [                   // Users to skip
     "auto_moderator",
     "Dari",
-    "DasaDevil",
     "Varka",
+]
+const zeroExpectations = [              // Users who are not expected to handle approvals
+    "Xch3l",
+    "Earlopain",
+    "NotMeNotYou",
+    "DasaDevil",
+    "Knotty_Curls",
 ]
 const scoreModifiers = {                // Score calculation parameters
     tickets: 1,
@@ -29,96 +34,105 @@ const Common = require("./_common");
 
     Common.setup();
     const whenInterval = Common.formatInterval(Common.lookupWhen);
-    const expectedScore = Common.countDays(Common.lookupWhen) * expectation;
     Common.logName = outputName;
 
-
-    
-    if(Common.useCache && fs.existsSync(`./data/${outputName}.json`)) {
-
-        Common.log("=== Rebuilding CSV File ===");
-        const approverStats = JSON.parse(fs.readFileSync(`./data/${outputName}.json`));
-        let highestScore = 0,
-            secondHighest = 0;
-
-        for(const user of Object.values(approverStats)) {
-            user.score = calculateScore(user.tickets);
-
-            Common.log("", "Analyzing: " + user.name, `  Score: ${user.score}`);
-            if(user.score > highestScore) {
-                secondHighest = highestScore;
-                highestScore = user.score;
-                Common.logUpdate(`  Score: ${user.score} FIRST (${highestScore}, ${secondHighest})`);
-            } else if(user.score > secondHighest) {
-                secondHighest = user.score;
-                Common.logUpdate(`  Score: ${user.score} SECOND (${highestScore}, ${secondHighest})`);
-            }
-        }
-
-        printToFile(approverStats, secondHighest, expectedScore);
-        Common.log("", "=== CSV File Complete ===");
-
-        return;
-    }
-
-
     Common.log("=== Starting Lookup ===", "Period: " + whenInterval);
-    const approverStats = {};
-    let highestScore = 0,
-        secondHighest = 0;
 
-    // If we ever have more than 75 approvers, this will be a problem
-    const users = await fetch(`https://e621.net/users.json?search[min_level]=40`, Common.RequestOptions).then(response => response.json());
-    Common.log("Fetching data for " + users.length + " users");
+
+    Common.log("Moderators:");
+    const lookup = await fetch(`https://e621.net/users.json?search[min_level]=40`, Common.RequestOptions).then(response => response.json());
     Common.sleep();
 
+    let ticketHandlers = [],
+        userData = {},
+        userIndex = {},
+        statsBucket = {};
+    for(const user of lookup) {
+        userIndex[user.id] = user.name;
+        userData[user.name] = user;
 
-    for(const user of users) {
         if(exemptUsers.includes(user.name)) continue;
+        statsBucket[user.name] = 0;
 
-        Common.log("", "Analyzing: " + user.name);
-        let page = 0, results = 0;
+        if(!zeroExpectations.includes(user.name))
+            ticketHandlers.push(user.name);
+    }
+    Common.logUpdate(`Moderators: ${ticketHandlers.length}`);
 
-        Common.log(`  Tickets:`);
-        page = 0, results = 0;
-        do {
-            page++;
+    
 
-            let lookup = await fetch(`https://e926.net/mod_actions.json?search[action]=ticket_update&search[creator_name]=${user.name}&search[created_at]=${whenInterval}&page=${page}&limit=320`, Common.RequestOptions).then(response => response.json());
 
-            results = lookup.length;
-            Common.logUpdate(`  Tickets: page ${page}, results ${results}`);
+    Common.log("Finding tickets [page 1]:");
 
-            Common.sleep();
-        } while(results == 320)
-        const tickets = ((page - 1) * 320) + results;
-        Common.logUpdate(`  Tickets: ${tickets}`);
+    let results = 0,
+        page = 1,
+        totalTickets = 0;
 
-        const score = calculateScore(tickets);
-        Common.log(`  Score: ${score}`);
-        if(score > highestScore) {
-            secondHighest = highestScore;
-            highestScore = score;
-            Common.logUpdate(`  Score: ${score} FIRST (${highestScore}, ${secondHighest})`);
-        } else if(score > secondHighest) {
-            secondHighest = score;
-            Common.logUpdate(`  Score: ${score} SECOND (${highestScore}, ${secondHighest})`);
+    const unfoundUsers = new Set();
+
+    do {
+        let lookup = await fetch(`https://e926.net/mod_actions.json?search[action]=ticket_update&search[created_at]=${whenInterval}&page=${page}&limit=320`, Common.RequestOptions).then(response => response.json());
+        results = lookup.length;
+        totalTickets += results;
+
+        Common.logUpdate(`Finding tickets [page 1]:`);
+
+        for(const entry of lookup) {
+            let userName = userIndex[entry["creator_id"]];
+            if(typeof userName == "undefined") {
+                userName = entry["creator_id"];
+                unfoundUsers.add(entry["creator_id"]);
+            }
+
+            if(typeof statsBucket[userName] == "undefined")
+                statsBucket[userName] = 0;
+            else statsBucket[userName]++;
         }
-    
-        approverStats[user.name] = {
-            name: user.name,
-            id: user.id,
-            level: user.level,
-    
-            tickets: tickets,
-    
-            score: score,
-        };
 
-        printToFile(approverStats, secondHighest, expectedScore);
+        const message = [];
+        for(const [userName, ticketNumber] of Object.entries(statsBucket))
+            message.push(`  ${userName}: ${ticketNumber}`);
+            Common.logUpdate(`Finding tickets [page 1]:\n  ` + message.join("\n  "));
+
+        page++;
+        Common.sleep();
+    } while(results == 320);
+
+    if(unfoundUsers.size > 0) {
+        Common.log(`Unidentified users: ${unfoundUsers.size}`);
+
+        const lookup = await fetch(`https://e621.net/users.json?search[id]=${Array.from(unfoundUsers).join(",")}`, Common.RequestOptions).then(response => response.json());
+        Common.sleep();
+
+        for(const user of lookup) {
+            userIndex[user.id] = user.name;
+            userData[user.name] = user;
+    
+            if(exemptUsers.includes(user.name)) {
+                delete statsBucket[user.id];
+                continue;
+            }
+            statsBucket[user.name] = statsBucket[user.id];
+            delete statsBucket[user.id];
+    
+            if(!zeroExpectations.includes(user.name))
+                ticketHandlers.push(user.name);
+        }
     }
 
-    printToFile(approverStats, secondHighest, expectedScore);
+    const approverStats = {};
+    for(const [userName, ticketCount] of Object.entries(statsBucket)) {
+        approverStats[userName] = {
+            name: userName,
+            id: userData[userName].id,
+            level: userData[userName].level,
+
+            tickets: ticketCount,
+            score: calculateScore(ticketCount),
+        };
+    }
+
+    printToFile(approverStats, Math.round(calculateScore(totalTickets) / ticketHandlers.length));
     Common.log("\n=== Lookup Finished ===");
 })();
 
@@ -127,53 +141,59 @@ function calculateScore(tickets) {
     return scoreModifiers.tickets * tickets;
 }
 
-function calculateLetterGrade(score, target) {
-    const percent = Math.round((score / target) * 100);
-    if(percent >= 100) return "A";
-    if(percent >= 70) return "B";
-    if(percent >= 40) return "C";
-    if(percent >= 10) return "D";
-    return "F";
-}
-
-function calculateLetterRanking(score, highest) {
-    if(score > highest) return "S";
-
-    const percent = Math.round((score / highest) * 100);
-    if(percent >= 80) return "A";
-    if(percent >= 60) return "B";
-    if(percent >= 40) return "C";
-    if(percent >= 20) return "D";
-    return "F";
-}
-
-function printToFile(approverStats, secondHighest, expectedScore) {
+function printToFile(approverStats, expectedScore) {
 
     const printData = [];
     printData.push([
-        "Username",
+
+        // Publicly Visible
+        Common.lookupWhen.toLocaleDateString("en-us", {month: "long", year: "numeric"}),
+        "Tickets",
+        "Grade",
+        "Trend",
+        " ",
+
+        // Histogram
+        `"=TEXT(DATE(YEAR(A1), MONTH(A1) - 2, 0), ""mmmm"")"`,
+        `"=TEXT(DATE(YEAR(A1), MONTH(A1) - 1, 0), ""mmmm"")"`,
+        `"=TEXT(DATE(YEAR(A1), MONTH(A1), 0), ""mmmm"")"`,
+        `"=TEXT(DATE(YEAR(A1), MONTH(A1), 1), ""mmmm"")"`,
+        "Trend",
+        " ",
+
+        // Background info
         "ID",
         "Level",
-
-        "Tickets",
-
         "Score",
-        "Grade",
-        "Ranking",
+        "Inactive",
     ]);
 
+    let iterator = 2;
     for(const user of Object.values(approverStats)) {
         printData.push([
+
+            // Publicly Visible
             user.name,
+            user.tickets,
+            `"=IF(O${iterator},""X"", VLOOKUP($I$2:$I$52,Utility!$B$4:$C$24,2,TRUE))"`, // Grade calculations done in the spreadsheet
+            `"=IF(O${iterator}, """", VLOOKUP($J$2:$J$52,Utility!$E$4:$F$24,2,TRUE))"`, // Same for the tendency
+            "",
+
+            // Histogram
+            `"=IFERROR(XLOOKUP(A${iterator}, INDIRECT(F$1&""!A2:A""), INDIRECT(F$1&""!I2:I"")), 0)"`,
+            `"=IFERROR(XLOOKUP(A${iterator}, INDIRECT(G$1&""!A2:A""), INDIRECT(G$1&""!I2:I"")), 0)"`,
+            `"=IFERROR(XLOOKUP(A${iterator}, INDIRECT(H$1&""!A2:A""), INDIRECT(H$1&""!I2:I"")), 0)"`,
+            Math.round((user.score / expectedScore) * 100),
+            `"=I${iterator} - ROUND(AVERAGE(F${iterator}:H${iterator}))"`,
+            "",
+
+            // Background info
             user.id,
             user.level,
-
-            user.tickets,
-
             user.score,
-            calculateLetterGrade(user.score, expectedScore, true),
-            calculateLetterRanking(user.score, secondHighest),
+            zeroExpectations.includes(user.name) ? "TRUE" : "",
         ].join(","));
+        iterator++;
     }
 
     fs.writeFileSync(`./data/${outputName}.csv`, printData.join("\n"));
